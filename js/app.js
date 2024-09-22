07a073c7072d73df215872d9cfddcb89a3586e61
xrustik666 <kilorust@gmail.com>
2024-09-22 02:24:05 +0200

    works well

--- js/app.js
+++ js/app.js
@@ -1,8 +1,46 @@
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-ScrollSmoother.create({
-  wrapper: '.wrapper',
-  content: '.content',
-  smooth: 1.5,
-  effects: true,
-})
+if (ScrollTrigger.isTouch !== 1) {
+  ScrollSmoother.create({
+    wrapper: '.wrapper',
+    content: '.content',
+    smooth: 1.5,
+    effects: true,
+  })
+
+  gsap.fromTo('.hero-section', { opacity: 1 }, { 
+    opacity: 0,
+    scrollTrigger: {
+      trigger: '.hero-section',
+      start: '0%',
+      end: '70%',
+      scrub: true,
+    }
+  })
+
+  /* Animation for left gallery items */
+  const itemsL = document.querySelectorAll('.gallery__left .gallery__item');
+
+  itemsL.forEach(item => {
+    gsap.fromTo(item, { x: -100, opacity: 0 }, {
+      opacity: 1, x: 0,
+      scrollTrigger: {
+        trigger: item,
+        scrub: true,
+      }
+    })
+  })
+
+  /* Animation for right gallery items */
+  const itemsR = document.querySelectorAll('.gallery__right .gallery__item');
+
+  itemsR.forEach(item => {
+    gsap.fromTo(item, { x: +100, opacity: 0 }, {
+      opacity: 1, x: 0,
+      scrollTrigger: {
+        trigger: item,
+        scrub: true,
+      }
+    })
+  })
+}
